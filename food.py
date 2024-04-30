000838c268f6c7b6eebb1150e46acfed14d66a5d
ntrsai <ntrsai776@gmail.com>
2024-04-30 23:03:37 +0530

    first commit

---
+++ food.py
@@ -0,0 +1,18 @@
+food=input("choose your favourite dish veg/non-veg :")
+if food=='veg':
+    veg=input("which one food is your favourite of (veg-biryani,veg-soap,paneer-roti):")
+    if veg=='veg-biryani':
+        print("it is good taste ")
+    elif veg=='veg-soap':
+        print("yummy taste")
+    elif veg=='panner-roti':
+        print("good for health")
+        
+if food=='nonveg':
+    nonveg=input("which one food is your favourite of (chicken-biryani,chicken-soap,tandoor-prawns):")
+    if nonveg=='chicken-biryani':
+        print("it is good taste ")
+    elif nonveg=='chicken-soap':
+        print("yummy taste")
+    elif nonveg=='tandoor-prawns':
+        print("good for health")
