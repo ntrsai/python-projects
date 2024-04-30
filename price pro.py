589eb828ad694474291dc6749fff4d605b849b64
ntrsai <ntrsai776@gmail.com>
2024-04-30 21:26:04 +0530

    first commit

---
+++ price pro.py
@@ -0,0 +1,12 @@
+
+price=input("which price to want laptop:")
+if price>='30000' and price<='40000':
+    print("This price to you want dell laptop")
+elif price>='41000' and price<='50000':
+    print("This price to you want lenovo laptop")
+elif price>='51000' and price<='58000':
+    print("This price to you want apple laptop")
+elif price>='59,000' and price<='750000':
+    print("This price to you want mac laptop")
+elif price>='76,000' and price<='90000':
+    print("This price to you want hp laptop")
